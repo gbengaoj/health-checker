405a55b4d2b66f63bb532433e8fd0d92c6bce950
gbengaoj <gbengaoj@gmail.com>
2020-06-13 12:22:11 +0100

    Add flask implementation

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
 from flask import Flask, render_template, request
-#from flask_restful import Resource, Api
-#from json import dumps
-import requests
 
-from controller.healthservicehandler import HandleService
-
-#from healthchecks.services.users import UserService
-#from healthchecks.services.sms_notifications import SmsService
-#from healthchecks.services.manage_pages import ManagePageService
-#from healthchecks.services.email_notifications import EmailService
-#from healthchecks.services.company_settings import CompanySettingsService
-#from healthchecks.services.auth import AuthService
-#import threading
+from script.healthservicehandler import HandleService
 
 
 app = Flask(__name__)
@@ -60,51 +49,6 @@ def healthChecktimer():
                         </form>
                     </body>
                 </html>'''
-
-
-
-
-        
-        #return '''<h1>The language value is: {}</h1>
-                  #<h1>The framework value is: {}</h1>'''.format(language, framework)
-
-
-
-    
-    """
-    #print(AuthHandlerHealthCheck, UserhandlerHealthCheck)
-
-    # Authentication Microservices Health Check
-    AuthHandler = AuthService()
-    AuthHandlerHealthCheck = checker.process_service_check(AuthHandler.create_service_url())
-    
-    # Managing Users Microservices Health Check
-    Userhandler = UserService()
-    UserhandlerHealthCheck = checker.process_service_check(Userhandler.create_service_url())
-    
-    # SMS Notifications Microservices Health Check
-    SmsNotificationHandler = SmsService()
-    SmsNotificationHandlerHealthCheck = checker.process_service_check(SmsNotificationHandler.create_service_url())
-
-    # Managing Static and External Pages Microservices Health Check
-    ManagePageHandler = ManagePageService()
-    ManagePageHandlerHealthCheck = checker.process_service_check(ManagePageHandler.create_service_url())
-
-    # Email Notifications Microservices Health Check
-    EmailHandler = EmailService()
-    EmailHandlerHealthCheck = checker.process_service_check(EmailHandler.create_service_url())
-
-    # Managing Company Settings Microservices Health Check
-    CompanyHandler = CompanySettingsService()
-    CompanyHandlerHealthCheck = checker.process_service_check(CompanyHandler.create_service_url())
-    
-    services_status = (UserhandlerHealthCheck, SmsNotificationHandlerHealthCheck, ManagePageHandlerHealthCheck, EmailHandlerHealthCheck, CompanyHandlerHealthCheck, AuthHandlerHealthCheck)
-    for services in services_status:
-        print(services)
-    #print(AuthHandlerHealthCheck, UserhandlerHealthCheck)
-
-    healthChecktimer()
-    """
 
 
 if __name__ == "__main__":
--- settings.py
+++
@@ -1,8 +0,0 @@
-# Basic Configurations For the Backend To Work with Microservices
-
-auth_server_name = 'https://auth.microapi.dev/v1/'
-sms_server_name = 'https://sms.microapi.dev/v1/'
-company_server_name = 'https://company.microapi.dev/v1/'
-email_server_name = 'https://mailer.microapi.dev/v1/'
-pages_server_name = 'https://pages.microapi.dev/v1/'
-users_server_name = 'https://users.microapi.dev/v1/'
